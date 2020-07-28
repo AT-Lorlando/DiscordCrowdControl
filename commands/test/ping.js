093a750013c91a1eaf3959e3f007adc80bb489fe
AT-Lorlando <rcd.jeremy@gmail.com>
2020-07-28 17:47:27 +0200

    V 1.0.0

---
+++ commands/test/ping.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars */
+module.exports.run = (client, message, args) => {
+	message.channel.send('Pong!');
+};
+
+module.exports.help = {
+	name: 'ping',
+	description: 'Renvoie pong!',
+	args: false,
+	admin: false,
+};
---
+++ config.js
@@ -0,0 +1,5 @@
+exports.PREFIX = '!';
+exports.TOKEN = '';
+exports.IDADMIN = '';
+exports.IDPROF = '';
+exports.IDBOT = '';
